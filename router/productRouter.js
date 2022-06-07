f4a289416712eea85aedb2203da9fa3e323a8eda
Fabio <95591197>
2022-06-07 16:45:31 +0200

    add routes to all products and specific product

--- router/productRouter.js
+++ router/productRouter.js
@@ -12,4 +12,30 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const getOneProduct = await Product.findByPk(id, { raw: true });
+    res.send(getOneProduct);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+
+//     const getOneProduct = await Product.findAll({
+//       where: {
+//         id: id,
+//       },
+//     });
+//     res.send(getOneProduct);
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
+
 module.exports = router;
